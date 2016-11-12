13db9bb732eb518603f9f9fe2c873471aeac2dda
Dat <tdat.nguyen93@gmail.com>
2016-11-12 01:18:36 -0800

    Offically switched to Jekyll

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,13 +10,18 @@ if(empty($_POST['name'])  		||
 	return false;
    }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$name = $_POST['name'];
+$email_address = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+if ($email === FALSE) {
+    echo 'Invalid email';
+    exit(1);
+}
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
 
 // Create the email and send the message
-$to = 'tdat.nguyen93@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
